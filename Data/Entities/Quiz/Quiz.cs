fbeab0eed34c5079b3d9b707d56bfa58960bcd6f
ultrakhaicraft <ultrakhaicraft2004@gmail.com>
2025-04-16 09:51:38 +0700

    Add Quiz related Entity

---
+++ Data/Entities/Quiz/Quiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities.Quiz
+{
+	public class Quiz : BaseEntities
+	{
+		[Required(ErrorMessage ="Quiz name is required")]
+		public string Name { get; set; } = null!;
+
+		[Required(ErrorMessage = "Quiz description is required")]
+		public string Description { get; set; } = null!;
+
+		[Required(ErrorMessage = "Quiz type is required")]
+		public string Type { get; set; } = null!;
+	}
+}
---
+++ Data/Entities/Quiz/QuizRangeScore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities.Quiz
+{
+	public class QuizRangeScore : BaseEntities
+	{
+		public int MinScore { get; set; } = 0;
+		public int MaxScore { get; set; } = 0;
+
+		[Required(ErrorMessage = "Result is required")]
+		public string Result { get; set; } = null!;
+
+		//Foreign Keys
+		public Guid QuizId { get; set; }
+		public Quiz? Quiz { get; set; }
+	}
+}
