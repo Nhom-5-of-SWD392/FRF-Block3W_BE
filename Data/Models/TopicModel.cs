9b0bd1d35eaa094508a1c45dfe72c6a1ea3a40e4
ultrakhaicraft <ultrakhaicraft2004@gmail.com>
2025-04-16 21:40:42 +0700

    Create CRUD Topic

---
+++ Data/Models/TopicModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Data.Models;
+
+public class TopicViewModel : BaseModel
+{
+	[Required]
+	public string? Name { get; set; }
+}
+
+public class TopicCreateModel
+{
+	[Required]
+	public string? Name { get; set; }
+}
+
+public class TopicUpdateModel
+{
+	[Required]
+	public string? Name { get; set; }
+
+	[JsonIgnore]
+	public DateTime DateUpdate { get; set; } = DateTime.UtcNow;
+}
+
+public class TopicQueryModel : QueryStringParameters
+{
+	public TopicQueryModel()
+	{
+		OrderBy = "Name";
+	}
+
+	public string? Search { get; set; }
+}
---
+++ FRF Project_Block3W/Controllers/TopicController.cs
@@ -0,0 +1,68 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Service.Core;
+
+namespace FRF_Project_Block3W.Controllers;
+
+[Route("api/topic")]
+[ApiController]
+public class TopicController : ControllerBase
+{
+	private readonly ITopicService _topicService;
+
+	public TopicController(ITopicService topicService)
+	{
+		_topicService = topicService;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetAll([FromQuery] TopicQueryModel query)
+	{
+		var data = await _topicService.GetAll(query);
+		return Ok(data);
+	}
+
+	[HttpGet("id")]
+	public async Task<IActionResult> GetTopicDetailAsync(Guid id)
+	{
+		var data = await _topicService.GetTopicById(id);
+		if (data == null)
+		{
+			return NotFound();
+		}
+		return Ok(data);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> CreateTopic([FromBody] TopicCreateModel model)
+	{
+		if (!ModelState.IsValid)
+		{
+			return BadRequest();
+		}
+		var data = await _topicService.Create(model);
+		return Ok(data);
+	}
+
+	[HttpPut("id")]
+	public async Task<IActionResult> UpdateTopic(Guid id, [FromBody] TopicUpdateModel model)
+	{
+		if (!ModelState.IsValid)
+		{
+			return BadRequest();
+		}
+		var data = await _topicService.Update(id, model);
+		return Ok(data);
+	}
+
+	[HttpDelete("id")]
+	public async Task<IActionResult> DeleteTopic(Guid id)
+	{
+		var data = await _topicService.Delete(id);
+		if (data == null)
+		{
+			return NotFound();
+		}
+		return Ok(data);
+	}
+}
